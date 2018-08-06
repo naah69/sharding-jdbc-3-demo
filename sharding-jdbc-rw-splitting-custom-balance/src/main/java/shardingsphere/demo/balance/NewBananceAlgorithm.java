a3f770fed2ced5e4baf17670606cb641e4d0f51b
Naah <nayan3480232@163.com>
2018-08-06 15:07:41 +0800

    添加读写分离自定义负载均衡算法

---
+++ sharding-jdbc-rw-splitting-custom-balance/src/main/java/shardingsphere/demo/balance/NewBananceAlgorithm.java
@@ -0,0 +1,33 @@
+package shardingsphere.demo.balance;
+
+import io.shardingsphere.core.api.algorithm.masterslave.MasterSlaveLoadBalanceAlgorithm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * NewBananceAlgorithm
+ *
+ * @author naah
+ * @date 2018-08-06 下午2:26
+ * @desc
+ */
+public class NewBananceAlgorithm implements MasterSlaveLoadBalanceAlgorithm {
+    private final Logger logger= LoggerFactory.getLogger(NewBananceAlgorithm.class);
+    @Override
+    public String getDataSource(String name, String masterDataSourceName, List<String> slaveDataSourceNames) {
+        logger.info(name);
+        logger.info(masterDataSourceName);
+        logger.info(slaveDataSourceNames.toString());
+
+        if(LocalTime.now().isAfter(LocalTime.of(12,0,0))){
+            return slaveDataSourceNames.get(new Random().nextInt(slaveDataSourceNames.size()));
+        }else{
+            return slaveDataSourceNames.get(0);
+        }
+    }
+}
---
+++ sharding-jdbc-rw-splitting-custom-balance/src/test/java/shardingsphere/demo/DemoApplicationTests.java
@@ -0,0 +1,46 @@
+package shardingsphere.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import shardingsphere.demo.entity.Order;
+import shardingsphere.demo.mapper.OrderMapper;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+    @Autowired
+    private OrderMapper orderMapper;
+
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void add() {
+        Order order = new Order();
+        for (int i =0; i <=20; i++) {
+            for (int j = 0; j <= 30; j++) {
+                order.setUserId(i);
+                order.setOrderId(j);
+                orderMapper.insert(order);
+            }
+        }
+    }
+
+    @Test
+    public void getByUserId(){
+        List<Order> byUserId = orderMapper.findByUserId(20);
+        for (Order order : byUserId) {
+            System.out.println(order);
+        }
+
+    }
+
+
+}
