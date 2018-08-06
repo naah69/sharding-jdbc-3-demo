a0d03f2cacc9a2af6ae6be44dac4423cb9f9a7d6
Naah <nayan3480232@163.com>
2018-08-06 10:02:38 +0800

    更新修复zookeeper数据治理无线问题

--- sharding-jdbc-orchestration/src/main/java/shardingsphere/demo/DemoApplication.java
+++ sharding-jdbc-orchestration/src/main/java/shardingsphere/demo/DemoApplication.java
@@ -10,7 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         SpringApplication.run(DemoApplication.class, args);
+        Thread.sleep(Long.MAX_VALUE);
     }
 }
