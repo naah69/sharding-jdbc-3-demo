e0b87a818214b053ed08aed579b07576dafae2a0
Naah <nayan3480232@163.com>
2018-08-07 14:49:33 +0800

    修复模块之间关系

--- sharding-jdbc-rw-splitting/src/main/java/shardingsphere/demo/mapper/OrderMapper.java
+++ sharding-jdbc-rw-splitting/src/main/java/shardingsphere/demo/mapper/OrderMapper.java
@@ -1,11 +1,14 @@
 package shardingsphere.demo.mapper;
 
+import io.shardingsphere.core.api.algorithm.masterslave.MasterSlaveLoadBalanceAlgorithm;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 import shardingsphere.demo.entity.Order;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author fengsihang
@@ -41,3 +44,4 @@ public interface OrderMapper {
     List<Order> findByOrderId(Integer id);
 
 }
+
